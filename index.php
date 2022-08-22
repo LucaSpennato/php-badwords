365aa7c72879b390bcbdec4f10ce83593bdd5f6d
LucaSpennato <luca.spennato@gmail.com>
2022-08-22 15:34:30 +0200

    testings

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-
 // Creare una variabile con un paragrafo di testo a vostra scelta.
 // Stampare a schermo il paragrafo e la sua lunghezza.
 // Una parola da censurare viene passata dall'utente tramite parametro GET.
 // Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi 
 // (***) tutte le occorrenze della parola da censurare. 
+
+    $phpVar = 'hello there';
 
 ?>
 
@@ -20,7 +21,13 @@
 
     <body>
 
-    
+        <h1>PHP exercise</h1>
+
+        <h5>
+            <?php
+                echo $phpVar;
+            ?>
+        </h5>
 
     </body>
 
